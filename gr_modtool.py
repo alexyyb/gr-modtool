bae98631ba190bda7bfa32634a978b40293cbd67
Martin Braun <martin.braun@kit.edu>
2012-10-28 22:41:39 +0100

    info: Added

--- gr_modtool.py
+++ gr_modtool.py
@@ -685,9 +685,9 @@ class ModTool(object):
         self._file['qalib'] = os.path.join('lib', 'qa_%s.cc' % self._info['modname'])
         self._file['pyinit'] = os.path.join('python', '__init__.py')
         self._file['cmlib'] = os.path.join('lib', 'CMakeLists.txt')
-        self._file['cmgrc'] = os.path.join('lib', 'CMakeLists.txt')
+        self._file['cmgrc'] = os.path.join('grc', 'CMakeLists.txt')
         self._file['cmpython'] = os.path.join('python', 'CMakeLists.txt')
-        self._file['cminclude'] = os.path.join('lib', self._info['modname'], 'CMakeLists.txt')
+        self._file['cminclude'] = os.path.join('include', self._info['modname'], 'CMakeLists.txt')
         self._file['cmswig'] = os.path.join('swig', 'CMakeLists.txt')
 
 
@@ -764,6 +764,85 @@ class ModTool(object):
                 return fname
         return None
 
+
+### Info  module #############################################################
+class ModToolInfo(ModTool):
+    """ Create a new out-of-tree module """
+    name = 'info'
+    aliases = ('getinfo', 'inf')
+    def __init__(self):
+        ModTool.__init__(self)
+
+    def setup_parser(self):
+        " Initialise the option parser for 'gr_modtool.py info' "
+        parser = ModTool.setup_parser(self)
+        parser.usage = '%prog info [options]. \n Call %prog without any options to run it interactively.'
+        ogroup = OptionGroup(parser, "Info options")
+        ogroup.add_option("--python-readable", action="store_true", default=None,
+                help="Return the output in a format that's easier to read for Python scripts.")
+        parser.add_option_group(ogroup)
+        return parser
+
+    def setup(self):
+        # Won't call parent's setup(), because that's too chatty
+        (self.options, self.args) = self.parser.parse_args()
+
+    def run(self):
+        """ Go, go, go! """
+        out_info = {}
+        base_dir = os.path.abspath(self.options.directory)
+        if self._check_directory(base_dir):
+            out_info['base_dir'] = base_dir
+        else:
+            (up_dir, this_dir) = os.path.split(base_dir)
+            if os.path.splitext(up_dir)[1] == 'include':
+                up_dir = os.path.splitext(up_dir)[0]
+            if self._check_directory(up_dir):
+                out_info['base_dir'] = up_dir
+            else:
+                if self.options.python_readable:
+                    print '{}'
+                else:
+                    print "No module found."
+                sys.exit(0)
+        os.chdir(out_info['base_dir'])
+        out_info['modname'] = get_modname()
+        out_info['incdirs'] = []
+        mod_incl_dir = os.path.join(out_info['base_dir'], 'include')
+        if os.path.isdir(os.path.join(mod_incl_dir, out_info['modname'])):
+            out_info['incdirs'].append(os.path.join(mod_incl_dir, out_info['modname']))
+        else:
+            out_info['incdirs'].append(mod_incl_dir)
+        if (os.path.isdir(os.path.join(out_info['base_dir'], 'build'))
+                and os.path.isfile(os.path.join(out_info['base_dir'], 'CMakeCache.txt'))):
+            out_info['build_dir'] = os.path.join(out_info['base_dir'], 'build')
+        else:
+            for (dirpath, dirnames, filenames) in os.walk(out_info['base_dir']):
+                if 'CMakeCache.txt' in filenames:
+                    out_info['build_dir'] = dirpath
+                    break
+        try:
+            cmakecache_fid = open(os.path.join(out_info['build_dir'], 'CMakeCache.txt'))
+            for line in cmakecache_fid:
+                if line.find('GNURADIO_CORE_INCLUDE_DIRS:PATH') != -1:
+                    out_info['incdirs'] += line.replace('GNURADIO_CORE_INCLUDE_DIRS:PATH=', '').strip().split(';')
+                if line.find('GRUEL_INCLUDE_DIRS:PATH') != -1:
+                    out_info['incdirs'] += line.replace('GRUEL_INCLUDE_DIRS:PATH=', '').strip().split(';')
+        except IOError:
+            pass
+        if self.options.python_readable:
+            print str(out_info)
+        else:
+            self._pretty_print(out_info)
+
+    def _pretty_print(self, out_info):
+        """ Output the module info in human-readable format """
+        index_names = {'base_dir': 'Base directory',
+                       'modname':  'Module name',
+                       'build_dir': 'Build directory',
+                       'incdirs': 'Include directories'}
+        for key in out_info.keys():
+            print '%19s: %s' % (index_names[key], out_info[key])
 
 ### Add new block module #####################################################
 class ModToolAdd(ModTool):
@@ -2596,6 +2675,7 @@ class ModToolMakeXML(ModTool):
 
     def run(self):
         """ Go, go, go! """
+        print "Warning: This is an experimental feature. Don't expect any magic."
         # 1) Go through lib/
         if not self._skip_subdirs['lib']:
             files = self._search_files('lib', '*_impl.cc')
@@ -2635,7 +2715,7 @@ class ModToolMakeXML(ModTool):
                                'in_constructor': False})
         if os.path.isfile(os.path.join('grc', fname_xml)):
             # TODO add an option to keep
-            print "Overwriting existing GRC file."
+            print "Warning: Overwriting existing GRC file."
         grc_generator = GRCXMLGenerator(
                 modname=self._info['modname'],
                 blockname=blockname,
